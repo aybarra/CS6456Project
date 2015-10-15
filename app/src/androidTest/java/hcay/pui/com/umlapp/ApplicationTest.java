a8681c3cf167848b3bf681b78cabfb4e496aa3b7
Andy Ybarra <andy.ybarra@gatech.edu>
2015-10-15 18:01:41 -0400

    Adding initial files

---
+++ app/src/androidTest/java/hcay/pui/com/umlapp/ApplicationTest.java
@@ -0,0 +1,13 @@
+package hcay.pui.com.umlapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
