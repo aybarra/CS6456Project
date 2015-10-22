92e95a0a867dcba0ff8234e90d58159ef6ccd3dd
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-10-22 00:00:42 -0400

    Changed the id variable name to strokeID.

--- app/src/main/java/hcay/pui/com/recognizer/Point.java
+++ app/src/main/java/hcay/pui/com/recognizer/Point.java
@@ -3,12 +3,12 @@ package hcay.pui.com.recognizer;
 public class Point {
 
     public double x, y;
-    public int id;
+    public int strokeID;
 
-    public Point(double x, double y, int id) {
+    public Point(double x, double y, int strokeID) {
         this.x = x;
         this.y = y;
-        this.id = id;
+        this.strokeID = strokeID;
     }
 
 }
