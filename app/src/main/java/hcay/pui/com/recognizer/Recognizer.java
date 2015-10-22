6b4ab7ea254bb9bccaa5211324320f8b8da487e1
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-10-22 00:02:27 -0400

    Implemented the main 3-step recognition methods (greedy cloud match not implemented yet).

--- app/src/main/java/hcay/pui/com/recognizer/Recognizer.java
+++ app/src/main/java/hcay/pui/com/recognizer/Recognizer.java
@@ -4,26 +4,118 @@ import java.util.ArrayList;
 
 public class Recognizer {
 
+    /** Number of points to use for the re-sampled path. */
+    private static final int N = 64;
+
     public ArrayList<RecognizerResult> recognize(ArrayList<Point> points) {
         points = adjust(points);
+
+        // TODO: implement recognition routine
 
         return null;
     }
 
     private ArrayList<Point> resample(ArrayList<Point> points) {
-        return null;
+        double incrementLength = getTotalPathDistance(points) / (N - 1);
+
+        ArrayList<Point> newPoints = new ArrayList<>(N);
+        newPoints.add(points.get(0));
+
+        double currentDistance = 0;
+
+        for (int i = 1; i < points.size(); i++) {
+            Point prev = points.get(i - 1);
+            Point curr = points.get(i);
+            if (prev.strokeID == curr.strokeID) {
+                double d = getPointDistance(prev, curr);
+                if ((currentDistance + d) >= incrementLength) {
+                    double qx = prev.x + ((incrementLength - currentDistance) / d) * (curr.x - prev.x);
+                    double qy = prev.y + ((incrementLength - currentDistance) / d) * (curr.y - prev.y);
+                    Point q = new Point(qx, qy, curr.strokeID);
+                    newPoints.add(q);
+                    points.add(i, q);
+                    currentDistance = 0;
+                } else {
+                    currentDistance += d;
+                }
+            }
+        }
+
+        return newPoints;
     }
 
     private ArrayList<Point> scale(ArrayList<Point> points) {
-        return null;
+        double minX, minY, maxX, maxY;
+        minX = minY = Double.POSITIVE_INFINITY;
+        maxX = maxY = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+            minX = Math.min(minX, point.x);
+            minY = Math.min(minY, point.y);
+            maxX = Math.max(maxX, point.x);
+            maxY = Math.max(maxY, point.y);
+        }
+
+        double scale = Math.max(maxX - minX, maxY - minY);
+
+        for (int i = 0; i < points.size(); i++) {
+            Point point = points.get(i);
+            point.x = (point.x - minX) / scale;
+            point.y = (point.y - minY) / scale;
+        }
+
+        return points;
     }
 
     private ArrayList<Point> translate(ArrayList<Point> points) {
-        return null;
+        Point c = getCentroid(points);
+
+        for (int i = 0; i < points.size(); i++) {
+            Point p = points.get(i);
+            p.x -= c.x;
+            p.y -= c.y;
+        }
+
+        return points;
+    }
+
+    private Point getCentroid(ArrayList<Point> points) {
+        Point c = new Point(0, 0, -1);
+
+        for (int i = 0; i < points.size(); i++) {
+            Point p = points.get(i);
+            c.x += p.x;
+            c.y += p.y;
+        }
+
+        c.x /= N;
+        c.y /= N;
+
+        return c;
     }
 
     private ArrayList<Point> adjust(ArrayList<Point> points) {
         return translate(scale(resample(points)));
+    }
+
+    private double getPointDistance(Point p1, Point p2) {
+        double dx = p2.x - p1.x;
+        double dy = p2.y - p1.y;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    }
+
+    private double getTotalPathDistance(ArrayList<Point> points) {
+        double distance = 0;
+        for (int i = 1; i < points.size(); i++) {
+            Point prev = points.get(i - 1);
+            Point curr = points.get(i);
+            if (prev.strokeID == curr.strokeID) {
+                // add the distances between points for the same stroke
+                distance += getPointDistance(prev, curr);
+            }
+        }
+        return distance;
     }
 
 }
