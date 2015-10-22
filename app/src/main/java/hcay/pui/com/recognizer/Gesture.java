c03cd12e43939e8f5bac30929c9c72bd1ffebd5e
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-10-22 01:46:47 -0400

    Changed the class to enum, added a constructor to set the name and isShape value for the Gesture, and added several example cases (T, N, D) to test.

--- app/src/main/java/hcay/pui/com/recognizer/Gesture.java
+++ app/src/main/java/hcay/pui/com/recognizer/Gesture.java
@@ -1,11 +1,13 @@
 package hcay.pui.com.recognizer;
 
-public class Gesture {
+public enum Gesture {
+
+    T("T", false), N("N", false), D("D", false);
 
     public String name;
     public boolean isShape;
 
-    public Gesture(String name, boolean isShape) {
+    Gesture(String name, boolean isShape) {
         this.name = name;
         this.isShape = isShape;
     }
