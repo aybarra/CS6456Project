7927dbcc52765d77f70fff3fe6081817b796a8b2
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-10-24 03:55:58 -0400

    Added labels and a recognize button at the bottom, and added Recognizer.

--- PRecognizerTest/RecognizerTester.java
+++ PRecognizerTest/RecognizerTester.java
@@ -4,11 +4,14 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class RecognizerTester extends JPanel {
 
 	public RecognizerTester() {
 		super(new BorderLayout());
+
+		Recognizer recognizer = new Recognizer();
 
 		GesturePanel gesturePanel = new GesturePanel();
 		add(gesturePanel, BorderLayout.CENTER);
@@ -17,13 +20,32 @@ public class RecognizerTester extends JPanel {
 		JLabel currentDetectedGestureLabel = new JLabel("None");
 		detectedGestureLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 20));
 		currentDetectedGestureLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 20));
-		detectedGestureLabel.setBorder(new EmptyBorder(0, 0, 0, 20));
+		detectedGestureLabel.setBorder(new EmptyBorder(0, 0, 0, 10));
 
 		JPanel labelPanel = new JPanel();
 		labelPanel.add(detectedGestureLabel);
 		labelPanel.add(currentDetectedGestureLabel);
-		labelPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
-		add(labelPanel, BorderLayout.SOUTH);
+
+		JButton recognizeButton = new JButton("Recognize");
+		recognizeButton.addActionListener(e -> {
+			ArrayList<Point> points = gesturePanel.getPoints();
+			ArrayList<RecognizerResult> result = recognizer.recognize(points);
+			gesturePanel.clear();
+			if (result.isEmpty()) {
+				currentDetectedGestureLabel.setText("Failed to recognize");
+			} else if (result.size() == 1) {
+				RecognizerResult rr = result.get(0);
+				currentDetectedGestureLabel.setText(rr.gesture.name + String.format(" (Score: %.3f)", rr.score));
+			} else {
+				currentDetectedGestureLabel.setText(result.toString());
+			}
+		});
+
+		JPanel bottomPanel = new JPanel(new BorderLayout());
+		bottomPanel.add(labelPanel, BorderLayout.CENTER);
+		bottomPanel.add(recognizeButton, BorderLayout.EAST);
+		bottomPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 0));
+		add(bottomPanel, BorderLayout.SOUTH);
 	}
 
     private static void createAndShowGUI() {
