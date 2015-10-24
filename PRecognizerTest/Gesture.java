3e90166e22b127107800d6b73b6ffc5ad7f261e5
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-10-24 04:26:43 -0400

    Added new Gestures: ARROW, CLASSIFIER, and GENERALIZATION, and removed D.

--- PRecognizerTest/Gesture.java
+++ PRecognizerTest/Gesture.java
@@ -1,6 +1,6 @@
 public enum Gesture {
 
-    T("T", false), N("N", false), D("D", false);
+    T("T", false), N("N", false), ARROW("-->", true), CLASSIFIER("CLASSIFIER", true), GENERALIZATION("--|>", true);
 
     public String name;
     public boolean isShape;
