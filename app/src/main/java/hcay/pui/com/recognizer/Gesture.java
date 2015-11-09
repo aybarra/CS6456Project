c1d190b45897114d52826191a6663480dbb189e8
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-11-08 23:31:11 -0500

    Updated to include a variable for the corresponding image file name.

--- app/src/main/java/hcay/pui/com/recognizer/Gesture.java
+++ app/src/main/java/hcay/pui/com/recognizer/Gesture.java
@@ -2,14 +2,15 @@ package hcay.pui.com.recognizer;
 
 public enum Gesture {
 
-    NAVIGABLE("-->", true), BI_NAVIGABLE("<->", true), CLASSIFIER("[]", true), GENERALIZATION("--|>", true),
-    UNSPECIFIED("---", true), NON_NAVIGABLE("x", true), REALIZATION_CIRCLE("o", true), REALIZATION("o--", true), AGGREGATION("<>-", true),
-    COMPOSITION("</>-", true), DEPENDENCY("- ->", true), REALIZATION_DEPENDENCY("- -|>", true), REQUIRED("--C", true);
+    NAVIGABLE("navigable", "-->", true), BI_NAVIGABLE("bi_navigable", "<->", true), CLASSIFIER("classifier", "[]", true), GENERALIZATION("generalization", "--|>", true),
+    UNSPECIFIED("unspecified", "---", true), NON_NAVIGABLE("non_navigable", "x", true), REALIZATION_CIRCLE("realization_circle", "o", true), REALIZATION("realization", "--o", true), AGGREGATION("aggregation", "-<>", true),
+    COMPOSITION("composition", "-</>", true), DEPENDENCY("dependency", "- ->", true), REALIZATION_DEPENDENCY("realization_dependency", "- -|>", true), REQUIRED("required", "--C", true);
 
-    public String name;
+    public String imageName, name;
     public boolean isShape;
 
-    Gesture(String name, boolean isShape) {
+    Gesture(String imageName, String name, boolean isShape) {
+        this.imageName = imageName;
         this.name = name;
         this.isShape = isShape;
     }
