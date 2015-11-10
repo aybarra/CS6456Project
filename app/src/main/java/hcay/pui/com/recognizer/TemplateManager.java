2d4429850ad64a9b079538b69f3e61cc096b9bb4
Hyun Seo Chung <hyunseo0404@gmail.com>
2015-11-10 16:40:43 -0500

    Added resetTemplates method to reset the templates file in the external storage.

--- app/src/main/java/hcay/pui/com/recognizer/TemplateManager.java
+++ app/src/main/java/hcay/pui/com/recognizer/TemplateManager.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -55,6 +56,11 @@ public class TemplateManager {
 
         if (changed) saveTemplates(t, context);
         return t;
+	}
+
+	public static void resetTemplates(Context context) {
+		context.deleteFile(TEMPLATE_FILE_NAME);
+		load(context);
 	}
 
 	public static void addNewTemplate(Gesture gesture, ArrayList<Point> points) {
